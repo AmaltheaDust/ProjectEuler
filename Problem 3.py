edb44ab3b6eec09bcf098d8cc2e1f989e473fe07
MerlynnDust <anthony.angel@x2i.systems>
2018-11-19 13:23:48 -0500

    rethinking the prime checker as it's inefficient

--- Problem 3.py
+++ Problem 3.py
@@ -4,34 +4,41 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-numValue = 13195
+numValue = 600851475143
 
 import math
 
-for i in reversed(range(math.ceil(numValue)/2)):
-    if numValue % i == 0:
-        if isprime(i) == True:
-            print("The largest prime factor of " + str(numValue) + " is " str(i))
+
+
+def isPrime(n):
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+    if n % 5 == 0:
+        return False
+    if n % 7 == 0:
+        return False
+    if n % 11 == 0:
+        return False
+    if n % 13 == 0:
+        return False
+
+
+    for i in reversed(range(2,int(n**0.5)+1)):
+        if n%i==0:
+            return False
+
+    return True
+
+
+
+for i in reversed(range(int(math.ceil(numValue)/2))):
+    print(i)
+    if i != 0 and numValue % i == 0:
+        if isPrime(i) == True:
+            print(i)
 
             break
-        else:
-            continue
-
-def isprime(aNumber):
-    if aNumber % 2 == 0:
-        return False
-    if aNumber % 3 == 0:
-        return False
-    if aNumber % 5 == 0:
-        return False
-    if aNumber % 7 == 0:
-        return False
-    if aNumber % 11 == 0:
-        return False
-
-    else:
-        for i in math.ceil(aNumber/2):
-            if (aNumber/2) % i == 0 and i != 1:
-                return True
-            else:
-                return True
+        #else:
+        #    print(i)
