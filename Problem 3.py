b3443ffaf084d7b1363f9e69e7f46803dfcf6f01
MerlynnDust <anthony.angel@x2i.systems>
2018-11-19 19:41:20 -0500

    made the focus more about the factors themselves, and isprime just checks if there are factors other than 1. Attempting my first try at multithreading, as well as using multiple objects to avoid interacting with the same iteration values at the same times.

--- Problem 3.py
+++ Problem 3.py
@@ -7,10 +7,10 @@ What is the largest prime factor of the number 600851475143 ?
 numValue = 600851475143
 
 import math
-
-
-
-def isPrime(n):
+import threading
+import sys
+"""
+def isPrimeFactor(n):
     if n % 2 == 0:
         return False
     if n % 3 == 0:
@@ -24,21 +24,77 @@ def isPrime(n):
     if n % 13 == 0:
         return False
 
-
-    for i in reversed(range(2,int(n**0.5)+1)):
-        if n%i==0:
-            return False
-
-    return True
+"""
 
 
 
-for i in reversed(range(int(math.ceil(numValue)/2))):
-    print(i)
-    if i != 0 and numValue % i == 0:
-        if isPrime(i) == True:
-            print(i)
+divisor_list = []
 
-            break
-        #else:
-        #    print(i)
+class highestPrimeFactors:
+
+
+
+    def allFactors(self, n):
+
+        global divisor_list
+
+        divisor_num = 2
+
+        iterate_divisor = 2
+
+        test_divisor_num = 0
+
+
+
+        while divisor_num > 1:
+
+            divisor_num = int(math.floor(n / iterate_divisor))
+            #print(divisor_num)
+            if n % divisor_num == 0:
+
+
+
+                if test_divisor_num != divisor_num:
+
+                    print(divisor_num)
+
+                    divisor_list.append(divisor_num)
+
+                test_divisor_num = divisor_num
+
+                #return True
+
+            iterate_divisor += 1
+
+        return divisor_list
+
+    def isPrime(self, n):
+        if self.allFactors(n) == [1]:
+            print("This number is prime")
+            return True
+
+    def highestPrimeFactor(self):
+
+        global divisor_list
+
+        while True:
+            for i in divisor_list:
+                if self.isPrime(i) == True:
+                    print("This is the highest prime factor")
+                    sys.exit()
+
+
+
+
+object1 = highestPrimeFactors()
+object2 = highestPrimeFactors()
+
+
+t1 = threading.Thread(target=object1.isPrime, args = (13,))
+t2 = threading.Thread(target=object2.highestPrimeFactor)
+
+t1.start()
+t2.start()
+
+#object1.isPrime(13)
+
