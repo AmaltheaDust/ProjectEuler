c324550e79382391a5442da1479ad3d398fbfdd2
Anthony Angel <aangel@gls.com>
2019-11-13 16:32:06 -0500

    added problem 17 but no answer yet as it looks tedious rather than difficult.

--- Problem 17.py
+++ Problem 17.py
@@ -4,3 +4,14 @@ If the numbers 1 to 5 are written out in words: one, two, three, four, five, the
 If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
 """
+
+firstnine_single_d = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+exception_11_19 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+firstnine_double_d = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+firstnine_triple_d = ['hundred']
+and_holder = ['and']
+
+
+for belowten in range(9):
+    print(firstnine_single_d[belowten])
---
+++ Problem 4 Part 2.py
@@ -0,0 +1,5 @@
+values = [999, 999, 999]
+
+for i in range(len(values)):
+    for i2 in range(values[i]):
+        print(i2)
